70e94126ec354b27d23af7fbb1f4eb711695b1cd
Ubuntu <ubuntu@ip-172-31-23-84.ec2.internal>
2019-09-16 14:58:27 +0000

    Added chat textbox functionality and refactored code

--- .~c9_invoke_L92YV.py
+++ .~c9_invoke_L92YV.py
@@ -1,16 +1,20 @@
 import os
 from datetime import datetime
-from flask import Flask, redirect, render_template, request, session, url_for
+from flask import Flask, redirect, render_template, request, session
 
 app = Flask(__name__)
 app.secret_key = "randomstring123"
 messages = []
 
 
-def add_message(username, message):
+def add_messages(username, message):
     """Add messages to the `messages` list"""
     now = datetime.now().strftime("%H:%M:%S")
-    messages.append({"timestamp": now, "from": username, "message": message})
+    messages_dict = {"timestamp": now, "from": username, "message": message}
+    
+    messages.append(messages_dict)
+
+
 
 @app.route("/", methods=["GET", "POST"])
 def index():
@@ -19,23 +23,26 @@ def index():
         session["username"] = request.form["username"]
         
     if "username" in session:
-        return redirect(url_for("user", username=session["username"]))
+        return redirect(session["username"])
         
     return render_template("index.html")
 
 
-@app.route("/chat/<username>", methods = ["GET", "POST"])
+@app.route("/<username>", methods= ["GET", "POST"])
 def user(username):
-    """Add and display chat messages"""
+    """Display chat messages"""
     
-    if request.method == "POST":
+    if request.method == "POST"
         username = session["username"]
-        message = request.form["message"]
-        add_message(username, message)
-        return redirect(url_for("user", username=session["username"]))
-        
+        message = request.form["mesag"]
     return render_template("chat.html", username= username, 
         chat_messages = messages)
+
+
+@app.route("/<username>/<message>")
+def send_message(username, message):
+    add_messages(username, message)
+    return redirect("/" + username)
 
 
 app.run(host=os.getenv("IP"), port=int(os.getenv("PORT")), debug=True)
